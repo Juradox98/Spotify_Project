de6f0b8f1394704f088701c78f1760ab3a2cce48
Joel Lozano <joel.lozano@rappi.com>
2023-06-20 11:21:41 -0600

    login

--- index.mjs
+++ index.mjs
@@ -1,3 +1,6 @@
+const clientId = 'd4c5a153520c4108a09b085d478cb21e';
+const redirectUri = 'http://127.0.0.1:5500/Data/spotyV2/playlist.html';
+
 function generateRandomString(length) {
     let text = '';
     let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -22,27 +25,26 @@ function generateRandomString(length) {
   
     return base64encode(digest);
   }
-  
-const clientId = 'd4c5a153520c4108a09b085d478cb21e';
-const redirectUri = 'http://127.0.0.1:5500/Data/spotyV2/playlist.html';
 
 let codeVerifier = generateRandomString(128);
 
-generateCodeChallenge(codeVerifier).then(codeChallenge => {
-  let state = generateRandomString(16);
-  let scope = 'user-read-private user-read-email';
-
-  localStorage.setItem('code_verifier', codeVerifier);
-
-  let args = new URLSearchParams({
-    response_type: 'code',
-    client_id: clientId,
-    scope: scope,
-    redirect_uri: redirectUri,
-    state: state,
-    code_challenge_method: 'S256',
-    code_challenge: codeChallenge
+function login() {
+  generateCodeChallenge(codeVerifier).then(codeChallenge => {
+    let state = generateRandomString(16);
+    let scope = 'user-read-private user-read-email';
+  
+    localStorage.setItem('code_verifier', codeVerifier);
+  
+    let args = new URLSearchParams({
+      response_type: 'code',
+      client_id: clientId,
+      scope: scope,
+      redirect_uri: redirectUri,
+      state: state,
+      code_challenge_method: 'S256',
+      code_challenge: codeChallenge
+    });
+  
+    window.location = 'https://accounts.spotify.com/authorize?' + args;
   });
-
-  window.location = 'https://accounts.spotify.com/authorize?' + args;
-});
+}
--- playlist.mjs
+++ playlist.mjs
@@ -1,3 +1,4 @@
+var playListFromSpoty;
 let codeVerifier = localStorage.getItem('code_verifier');
 
 const urlParams = new URLSearchParams(window.location.search);
@@ -26,15 +27,18 @@ const response = fetch('https://accounts.spotify.com/api/token', {
     return response.json();
   })
   .then(data => {
+    console.log("token: ", data);
     localStorage.setItem('access_token', data.access_token);
     getProfile()
   })
   .catch(error => {
     console.error('Error:', error);
   });
+
+  console.log(response);
 }
 
-  async function getProfile(accessToken) {
+  async function getProfile() {
     let accessToken = localStorage.getItem('access_token');
   
     const response = await fetch('https://api.spotify.com/v1/playlists/5nRcIY7BnEqgzrTH3GehH2', {
@@ -42,10 +46,48 @@ const response = fetch('https://accounts.spotify.com/api/token', {
         Authorization: 'Bearer ' + accessToken
       }
     });
-  
     const data = await response.json();
-    console.log(data);
+    playListFromSpoty = data
+    showTable()
+    showCards()
   }
-
   updateToken()
   
+  
+function showTable() {
+  document.getElementById('playListTitle').textContent = playListFromSpoty.name
+  
+  playListFromSpoty.tracks.items.forEach(cancion => {
+    const tr = document.createElement('tr')
+    const tdCancion = document.createElement('td')
+    const tdArtis = document.createElement('td')
+    const tdAlbum = document.createElement('td')
+
+    tdCancion.textContent = cancion.track.name
+    cancion.track.artists.forEach(artista => {
+      tdArtis.textContent = artista.name
+    })
+
+    console.log(cancion.track.album.name);
+    tdAlbum.textContent = cancion.track.album.name
+
+    tr.appendChild(tdCancion)
+    tr.appendChild(tdArtis)
+    tr.appendChild(tdAlbum)
+
+    document.getElementById('playlist-content').appendChild(tr)
+
+  });
+
+}
+
+function showCards() {
+  playListFromSpoty.tracks.items.forEach(cancion => {
+    let div = document.createElement('div').setAttribute('class', 'card')
+  let h5 = document.createElement('h5').setAttribute('class', 'card-title').textContent = cancion.track.name
+
+  div.appendChild(h5)
+  
+  document.getElementById('cardContent').appendChild(div)
+  })
+}
